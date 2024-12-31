d4128ae074efa0bf011ffa3d266f0934dbdd4dcd
Barbara <subwaynaas@yahoo.ie>
2024-12-31 14:59:20 +0200

    added account balance

--- app.js
+++ app.js
@@ -2,55 +2,109 @@ const messageEl = document.getElementById("message-el");
 const cardsEl = document.getElementById("cards-el");
 const sumEl = document.getElementById("sum-el");
 const accountBalanceEl = document.getElementById("account-balance-el");
+const cardDetailsEl = document.getElementById("card-details");
 
 let cards = [];
 let sum = 0;
 let message = "";
 let accountBalance = 1000;
-let hasBlackJack = false;
-let isAlive = false;
+
+document.getElementById("new-card-btn").disabled = true;
 
 accountBalanceEl.textContent = `Account Balance: $${accountBalance}`;
 
 //!  ---------  EVENT LISTENERS  ----------
 document.addEventListener("click", function (e) {
   if (e.target.id === "start-btn") {
-    startGame();
+    handleStartGame();
   } else if (e.target.id === "new-card-btn") {
-    newCard();
+    handleNewCard();
+  } else if (e.target.id === "close-btn") {
+    handleCloseBtn();
+  } else if (e.target.id === "top-up-btn") {
+    handleTopUp();
   }
+});
+
+cardDetailsEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(cardDetailsEl);
+  messageEl.style.color = "white";
+  messageEl.style.backgroundColor = "transparent";
+
+  messageEl.textContent = " Want to play a round? Each round costs $100";
+
+  cardDetailsEl.style.display = "none";
+
+  document.getElementById("confirmation-note").style.display = "block";
+
+  document.getElementById("confirmation-note").textContent = `${formData.get(
+    "name"
+  )}, you have successfully added $${formData.get("amount")} to your account.`;
+
+  setInterval(function () {
+    document.getElementById("confirmation-note").style.display = "none";
+  }, 3000);
+  messageEl.style.color = "white";
+  messageEl.style.backgroundColor = "transparent";
+
+  messageEl.textContent = " Want to play a round? Each round costs $100";
+  sum = 0;
+  cards = [];
+  cardsEl.textContent = `Cards: ${cards}`;
+  sumEl.textContent = `Sum: `;
+  // console.log({ cards, sum });
+
+  accountBalance += Number(formData.get("amount"));
+  accountBalanceEl.style.color = "rgb(164, 162, 162)";
+  accountBalanceEl.textContent = `Account Balance: $${accountBalance}`;
+
+  document.getElementById("new-card-btn").disabled = true;
+  document.getElementById("start-btn").disabled = false;
 });
 
 //!  --------- FUNCTIONS FOR EVENT LISTENERS ----------
 
-function startGame() {
-  isAlive = true;
+function handleStartGame() {
+  document.getElementById("confirmation-note").style.display = "none";
+
+  document.getElementById("start-btn").disabled = false;
+  document.getElementById("new-card-btn").disabled = false;
   let firstCard = getRandomCard();
   let secondCard = getRandomCard();
   cards = [firstCard, secondCard];
   sum = firstCard + secondCard;
   cardsEl.textContent += ` ${firstCard}  ${secondCard}`;
-  accountBalance = accountBalance - 100;
-  accountBalanceEl.textContent = `Account Balance: $${accountBalance}`;
+  messageEl.style.color = "white";
 
-  // must have this or else user can still Start Game - do NOT set to <= 0 or else user can't get a new card on their last game
   if (accountBalance < 0) {
-    isAlive = false;
+    accountBalance = 0;
+  } else if (accountBalance > 0) {
+    accountBalance = accountBalance - 100;
+  } else if (accountBalance === 0) {
+    cardDetailsEl.style.display = "flex";
   }
+
+  accountBalanceEl.textContent = `Account Balance: $${accountBalance}`;
 
   renderGame();
 }
 
-function newCard() {
-  //stop it from working if you get blackJack or go BUST
-  // only run it if isAlive = true && hasBlackjack = false
-  if (isAlive && !hasBlackJack) {
-    let newCard = getRandomCard();
-    sum += newCard;
-    renderGame();
-    cards.push(newCard);
-    cardsEl.textContent += ` ${newCard}`;
-  }
+function handleNewCard() {
+  let newCard = getRandomCard();
+  sum += newCard;
+  renderGame();
+  cards.push(newCard);
+  cardsEl.textContent += ` ${newCard}`;
+}
+
+function handleCloseBtn() {
+  cardDetailsEl.style.display = "none";
+}
+
+function handleTopUp() {
+  cardDetailsEl.style.display = "flex";
 }
 //!  --------- OTHER FUNCTIONS ----------
 
@@ -61,36 +115,51 @@ function renderGame() {
 
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
-    messageEl.style.color = "black";
-    messageEl.style.fontSize = "1.3rem";
   } else if (sum === 21) {
-    messageEl.style.color = "green";
-    messageEl.style.fontSize = "2rem";
-    message = `Congratulations, YOU HAVE BLACKJACK! We will add $1000 to your account the next time you play a new game.`;
-    accountBalance += 1000; //! how come this doesn't show up in the account until I click the START GAME btn again??
-    hasBlackJack = true;
-    isAlive = false; //? or else user can choose a newCard
-    //? BUT now when the user gets Blackjack they can't get a NEW CARD b/c is now True and we set up the newCard function to only work if the user !hasBlackJack - SO MUST WE CHANGE THIS TO hasBlackJack = false?? or maybe we reset the game but then we must set the account balance in local storage so it remembers the amount ???
-  } else if (sum > 21 && accountBalance <= 0) {
-    accountBalance = 0; // so we can't go into negative numbers
-    //? and make it so that the user can't Start Game but isAlive = false should have fixed that but it hasn't
-    accountBalanceEl.style.color = "red"; //this works
-    messageEl.style.fontSize = "2rem"; //this works
-    messageEl.style.color = "red"; //this works
-    message = `You have gone BUST & your account balance is ${accountBalance}. You must top up your account if you wish to play a new game`; //this works
-    isAlive = false; // this works b/c we can't get a New Card
-    //! BUT WE CAN STILL START GAME can we disable a btn??
-    document.getElementById("start-btn").disabled = true;
-  } else if (sum > 21 && accountBalance > 1) {
-    messageEl.style.color = "black";
-    messageEl.style.fontSize = "1.3rem";
-    message = `You have gone BUST. Try again if you are brave enough!`;
-    isAlive = false;
-  } else if (sum === 21 && accountBalance > 1) {
-    startGame();
-    isAlive = true;
-  }
+    if (accountBalance > 1) {
+      messageEl.style.color = "yellow";
 
+      message = `Congratulations, YOU HAVE BLACKJACK! We have added $1000💰️ to your account.`;
+
+      accountBalance += 1000;
+      accountBalanceEl.textContent = `Account Balance: $${accountBalance}`;
+
+      document.getElementById("new-card-btn").disabled = true;
+      document.getElementById("start-btn").disabled = false;
+
+      cards = [];
+      sum = 0;
+    } else if (accountBalance < 1) {
+      messageEl.style.color = "yellow";
+
+      message = `Congratulations, YOU HAVE BLACKJACK! We have added $1000💰️ to your account.`;
+      accountBalance += 1000;
+      accountBalanceEl.textContent = `Account Balance: $${accountBalance}`;
+
+      document.getElementById("new-card-btn").disabled = true;
+      document.getElementById("start-btn").disabled = false;
+    }
+  } else if (sum > 21) {
+    if (accountBalance <= 0) {
+      accountBalance = 0; // so we can't go into negative numbers
+
+      accountBalanceEl.style.color = "red";
+      messageEl.style.color = "white";
+      messageEl.style.backgroundColor = "red";
+      messageEl.style.padding = "1rem";
+      message = `You have gone BUST & your account balance is ${accountBalance}. You must top up your account if you wish to play a new game.`;
+
+      document.getElementById("start-btn").disabled = true;
+      document.getElementById("new-card-btn").disabled = true;
+
+      cardDetailsEl.style.display = "flex";
+    } else if (accountBalance > 1) {
+      messageEl.style.fontSize = "1.3rem";
+      message = `You have gone BUST. Try again if you are brave enough!`;
+
+      document.getElementById("new-card-btn").disabled = true;
+    }
+  }
   messageEl.textContent = message;
 }
 
@@ -99,13 +168,13 @@ function getRandomCard() {
   //ace should be 11 and 10, Jack, Queen and King should be 10
   if (randomNumber === 1) {
     return 11;
-  } else if (randomNumber > 10) {
+  } else if (randomNumber > 9) {
     return 10;
   } else {
     return randomNumber;
   }
 }
 
-//! PROBLEMS TO FIX:
-//! Everything that I set up in the renderGame if statements is delayed to next time the user clicks the start game btn
-//! when user wins and gets $1000 - and user starts new game - the new card doesn't work
+//? THINGS STILL TO DO:
+
+//! set up local Storage for accountBalance
